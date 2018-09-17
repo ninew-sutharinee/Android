766ae459ba901caae785994293d8d902f44e735f
ninew-sutharinee <43290327+ninew-sutharinee@users.noreply.github.com>
2018-09-17 21:36:22 +0700

    Add files via upload

---
+++ week3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.win10v3.myapplication;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void  add_text(){
+
+    }
+
+}
